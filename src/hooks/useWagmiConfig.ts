071c792ab305c2b90cf78fc51daa77f086c89848
jimmyyy <jimmy1141450313@gmail.com>
2024-11-19 15:54:15 +0800

    update

--- src/hooks/useWagmiConfig.ts
+++ src/hooks/useWagmiConfig.ts
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { cookieStorage, createStorage, http } from 'wagmi';
-import { mainnet, bscTestnet, sepolia, base } from 'wagmi/chains';
+import { mainnet, bscTestnet, sepolia, base, arbitrum } from 'wagmi/chains';
 import { tomoConnector } from '@/connectors/tomoConnector';
 import { type Chain } from 'wagmi/chains';
 
@@ -24,7 +24,7 @@ export function useWagmiConfig() {
     if (typeof window === 'undefined') return;
 
     // 定义为只读数组类型
-    const chains = [mainnet, sepolia, bscTestnet, base] as const;
+    const chains = [mainnet, sepolia, bscTestnet, base, arbitrum] as const;
     
     // 转换为普通数组传递给 tomoConnector
     const chainArray: Chain[] = Array.from(chains);
@@ -46,6 +46,7 @@ export function useWagmiConfig() {
         [sepolia.id]: http(),
         [bscTestnet.id]: http('https://data-seed-prebsc-1-s1.binance.org:8545'),
         [base.id]: http(),
+        [arbitrum.id]: http(),
       },
       storage: createStorage({ storage: cookieStorage }),
       enableWalletConnect: false,
