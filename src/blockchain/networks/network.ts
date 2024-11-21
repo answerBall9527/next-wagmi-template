3aa2c3898b5e7d2b13f65ea515d83b7321b90e04
jimmyyy <jimmy1141450313@gmail.com>
2024-11-21 15:05:56 +0800

    排查transfer error

--- src/blockchain/networks/network.ts
+++ src/blockchain/networks/network.ts
@@ -5,6 +5,10 @@ const networks: Networks = {
     contract: '0x123410253',
     token: '0xFa60D973F7642B748046464e165A65B7323b0DEE',
   },
+  1: {
+    contract: '0x123410253',
+    token: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  }
 };
 
 export const getNetwork = (chainId?: number): Network => {
