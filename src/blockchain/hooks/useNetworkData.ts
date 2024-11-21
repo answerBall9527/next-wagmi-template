ee7660c797885b86a45a2c2bf6b887b8209b828b
jimmyyy <jimmy1141450313@gmail.com>
2024-11-21 15:01:01 +0800

    排查transfer error

--- src/blockchain/hooks/useNetworkData.ts
+++ src/blockchain/hooks/useNetworkData.ts
@@ -6,6 +6,7 @@ import { Network } from '@/lib/types/network';
 
 const useNetworkData = (): Network => {
   const { chainId } = useAccount();
+  console.log('chainId', chainId)
   const networkData = getNetwork(chainId);
   return networkData as Network;
 };
