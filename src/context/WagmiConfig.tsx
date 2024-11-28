7284f0c382d8ad3bbd307f2059c86fc771921716
jimmyyy <jimmy1141450313@gmail.com>
2024-11-28 20:26:20 +0800

    change wallet connector parameter

--- src/context/WagmiConfig.tsx
+++ src/context/WagmiConfig.tsx
@@ -52,17 +52,7 @@ export function WagmiConfigProvider({ children }: { children: ReactNode }) {
           storage: cookieStorage,
           key: 'wagmi.wallet',
         }),
-        connectors: [connector, metaMask(), walletConnect({
-          projectId: '3fcc6bba6f1de962d911bb5b5c3dba68',
-          // relayUrl: 'wss://relay.walletconnect.com',  // WalletConnect 的官方中继服务器
-          metadata: {
-            name: 'pebbles',
-            description: 'pebbles',
-            url: 'https://next-wagmi-template.vercel.app/',
-            icons: ['https://static.stakestone.io/btc/babylon.png'],
-          },
-          showQrModal: true,
-        }), tomoInjectedConnector],
+        connectors: [connector, metaMask(), tomoInjectedConnector],
         syncConnectedChain: true,
         // logger: {
         //   warn: (message) => console.warn(message),
