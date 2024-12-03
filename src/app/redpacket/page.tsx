6d5eb22cb325bdcb9093cbac9c2779911856c431
jimmyyy <jimmy1141450313@gmail.com>
2024-12-03 21:57:21 +0800

    获取用户头像和名字

--- src/app/redpacket/page.tsx
+++ src/app/redpacket/page.tsx
@@ -73,7 +73,7 @@ export default function RedPacketPage() {
 
     const donator = {
         username: 'User',
-        handle: '@id'
+        handle: ''
     };
 
     const amount = {
@@ -116,7 +116,7 @@ export default function RedPacketPage() {
                             <div className="font-medium">
                                 {user?.username ? `@${user.username}` : `${user?.first_name || 'User'}`}
                             </div>
-                            <div className="text-sm text-gray-500">{donator.handle}</div>
+                            <div className="text-sm text-gray-500">{user?.username ? user?.username : donator.handle }</div>
                         </div>
                     </div>
                     
